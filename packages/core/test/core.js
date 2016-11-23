dad5631f6479b1283af748129155d06ba50570d7
Dominik Lubański <dominik.lubanski@gmail.com>
2016-11-23 16:08:21 +0100

    TEST basic core and engine tests

--- packages/core/test/core.js
+++ packages/core/test/core.js
@@ -1,1 +1,98 @@
-it('TODO', () => {});
+import { define, CONTROLLER } from '../src/index';
+
+describe('define', () => {
+  let el;
+  let ctrl;
+
+  class Controller {
+    static get options() {
+      return {
+        properties: ['test'],
+      };
+    }
+
+    constructor() {
+      this.test = 'test';
+      this.internal = 'test';
+    }
+  }
+
+  define('hybrids-core-test', Controller);
+
+  beforeEach(() => {
+    el = document.createElement('hybrids-core-test');
+    ctrl = el[CONTROLLER];
+  });
+
+  describe('constructor', () => {
+    it('initialize controller', () => {
+      expect(ctrl instanceof Controller).toEqual(true);
+    });
+
+    it('set public property', () => {
+      expect(el.test).toEqual('test');
+    });
+  });
+
+  describe('connected', () => {
+    beforeEach(() => {
+      document.body.appendChild(el);
+    });
+
+    afterEach(() => {
+      document.body.removeChild(el);
+    });
+
+    it('maps public property with controller', () => {
+      el.test = 'new value';
+      expect(ctrl.test).toEqual('new value');
+    });
+
+    it('maps controller property with element', () => {
+      ctrl.test = 'new value';
+      expect(el.test).toEqual('new value');
+    });
+
+    it('dispatch change event when public property has changed', (done) => {
+      const spy = jasmine.createSpy('callback');
+      el.addEventListener('change', spy);
+      el.test = 'new value';
+      window.requestAnimationFrame(() => {
+        expect(spy).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('dispatch change event when controller property has changed', (done) => {
+      const spy = jasmine.createSpy('callback');
+      el.addEventListener('change', spy);
+      ctrl.test = 'new value';
+      window.requestAnimationFrame(() => {
+        expect(spy).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('does not dispatch change event when controller internal property has changed', (done) => {
+      const spy = jasmine.createSpy('callback');
+      el.addEventListener('change', spy);
+      ctrl.internal = 'new value';
+      window.requestAnimationFrame(() => {
+        expect(spy).not.toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('dispatch change event for shadowRoot', (done) => {
+      const shadow = el.attachShadow({ mode: 'open' });
+      const spy = jasmine.createSpy('callback');
+      shadow.addEventListener('change', spy);
+      ctrl.internal = 'new value';
+
+      window.requestAnimationFrame(() => {
+        expect(spy).toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+});
--- packages/engine/test/engine.js
+++ packages/engine/test/engine.js
@@ -1,1 +1,46 @@
-it('TODO', () => {});
+import { define, CONTROLLER } from '@hybrids/core';
+import engine from '../src/engine';
+
+fdescribe('engine', () => {
+  let el;
+  let content;
+  // let ctrl;
+
+  define('hybrids-engine-test', class Controller {
+    static get options() {
+      return {
+        use: [engine],
+        template: `
+          <div>{{ test }}</div>
+        `
+      };
+    }
+
+    constructor() {
+      this.test = 'testing content';
+    }
+  });
+
+  beforeEach(() => {
+    el = document.createElement('hybrids-engine-test');
+    // ctrl = el[CONTROLLER];
+    content = el.shadowRoot.children[0];
+
+    document.body.appendChild(el);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(el);
+  });
+
+  it('renders view', () => {
+    expect(content.textContent.trim()).toEqual('testing content');
+  });
+
+  it('updates view', () => {
+    el.test = 'new value';
+    window.requestAnimationFrame(() => {
+      expect(content.textContent.trim()).toEqual('new value');
+    });
+  });
+});
