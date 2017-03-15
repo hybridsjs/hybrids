32fbe3dc4686e413886911f5d373137648f08614
Dominik Lubański <dominik.lubanski@gmail.com>
2017-03-15 11:17:16 +0100

    Fix ShadyDOM before Custom Elements polyfill

--- packages/shim/src/index.js
+++ packages/shim/src/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable global-require */
 require('@webcomponents/template');
 
-if (!window.customElements) require('@webcomponents/custom-elements');
 if (!document.body.attachShadow) require('@webcomponents/shadydom');
+if (!window.customElements) require('@webcomponents/custom-elements');
 
 require('@webcomponents/shadycss/scoping-shim.min');
 require('@webcomponents/shadycss/apply-shim.min');
