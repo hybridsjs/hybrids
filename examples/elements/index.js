ca3da4eb9155565f179b20bfec59bcf56712050b
Dominik Lubanski <dominik.lubanski@gmail.com>
2017-12-12 14:22:07 +0100

    Clear examples path & update packages

--- examples/elements/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ExCounter } from './ExCounter';
-export { default as ExTab } from './ExTab';
-export { default as ExTabGroup } from './ExTabGroup';
--- examples/examples.js
+++
@@ -1,6 +0,0 @@
-import { define } from '../src';
-import * as elements from './elements';
-
-import '../polyfill';
-
-define(elements);
