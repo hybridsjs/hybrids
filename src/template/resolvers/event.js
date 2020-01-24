e2dd413f7189d5498acc803eeea1b7ee84f26801
Dominik Lubanski <dominik.lubanski@gmail.com>
2020-01-24 09:38:11 +0100

    fix(html): fix replacing event listener callback

--- src/template/resolvers/event.js
+++ src/template/resolvers/event.js
@@ -1,13 +1,16 @@
-const eventMap = new WeakMap();
+const targets = new WeakMap();
 
 export default function resolveEventListener(eventType) {
   return (host, target, value, lastValue) => {
     if (lastValue) {
-      target.removeEventListener(
-        eventType,
-        eventMap.get(lastValue),
-        lastValue.options !== undefined ? lastValue.options : false,
-      );
+      const eventMap = targets.get(target);
+      if (eventMap) {
+        target.removeEventListener(
+          eventType,
+          eventMap.get(lastValue),
+          lastValue.options !== undefined ? lastValue.options : false,
+        );
+      }
     }
 
     if (value) {
@@ -15,11 +18,18 @@ export default function resolveEventListener(eventType) {
         throw Error(`Event listener must be a function: ${typeof value}`);
       }
 
-      eventMap.set(value, value.bind(null, host));
+      let eventMap = targets.get(target);
+      if (!eventMap) {
+        eventMap = new WeakMap();
+        targets.set(target, eventMap);
+      }
+
+      const callback = value.bind(null, host);
+      eventMap.set(value, callback);
 
       target.addEventListener(
         eventType,
-        eventMap.get(value),
+        callback,
         value.options !== undefined ? value.options : false,
       );
     }
