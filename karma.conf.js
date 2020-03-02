78a55ee2180340523d627da07122d9d67755a906
Dominik Lubanski <dominik.lubanski@gmail.com>
2020-03-02 15:47:53 +0100

    chore: update code styling

--- karma.conf.js
+++ karma.conf.js
@@ -1,85 +1,122 @@
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config");
 
-const IS_COVERAGE = process.env.NODE_ENV === 'coverage';
-const IS_SAUCE_LABS = !!process.env.TRAVIS && process.env.TRAVIS_BRANCH === 'master' && process.env.TRAVIS_PULL_REQUEST === 'false';
+const IS_COVERAGE = process.env.NODE_ENV === "coverage";
+const IS_SAUCE_LABS =
+  !!process.env.TRAVIS &&
+  process.env.TRAVIS_BRANCH === "master" &&
+  process.env.TRAVIS_PULL_REQUEST === "false";
 
 const customLaunchers = {
   SL_Chrome: {
-    base: 'SauceLabs', browserName: 'chrome', platform: 'Windows 10', version: 'latest',
+    base: "SauceLabs",
+    browserName: "chrome",
+    platform: "Windows 10",
+    version: "latest",
   },
   SL_Chrome_1: {
-    base: 'SauceLabs', browserName: 'chrome', platform: 'Windows 10', version: 'latest-1',
+    base: "SauceLabs",
+    browserName: "chrome",
+    platform: "Windows 10",
+    version: "latest-1",
   },
   SL_Firefox: {
-    base: 'SauceLabs', browserName: 'firefox', platform: 'Windows 10', version: 'latest',
+    base: "SauceLabs",
+    browserName: "firefox",
+    platform: "Windows 10",
+    version: "latest",
   },
   SL_Firefox_1: {
-    base: 'SauceLabs', browserName: 'firefox', platform: 'Windows 10', version: 'latest-1',
+    base: "SauceLabs",
+    browserName: "firefox",
+    platform: "Windows 10",
+    version: "latest-1",
   },
   SL_Safari: {
-    base: 'SauceLabs', browserName: 'safari', platform: 'macOS 10.13', version: 'latest',
+    base: "SauceLabs",
+    browserName: "safari",
+    platform: "macOS 10.13",
+    version: "latest",
   },
   SL_Safari_1: {
-    base: 'SauceLabs', browserName: 'safari', platform: 'macOS 10.13', version: 'latest-1',
+    base: "SauceLabs",
+    browserName: "safari",
+    platform: "macOS 10.13",
+    version: "latest-1",
   },
   SL_IE_11: {
-    base: 'SauceLabs', browserName: 'internet explorer', platform: 'Windows 8.1', version: '11',
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    platform: "Windows 8.1",
+    version: "11",
   },
   SL_EDGE: {
-    base: 'SauceLabs', browserName: 'microsoftedge', platform: 'Windows 10', version: 'latest',
+    base: "SauceLabs",
+    browserName: "microsoftedge",
+    platform: "Windows 10",
+    version: "latest",
   },
   SL_EDGE_1: {
-    base: 'SauceLabs', browserName: 'microsoftedge', platform: 'Windows 10', version: 'latest-1',
+    base: "SauceLabs",
+    browserName: "microsoftedge",
+    platform: "Windows 10",
+    version: "latest-1",
   },
   SL_iOS: {
-    base: 'SauceLabs', browserName: 'iphone', platform: 'macOS 10.13', version: 'latest',
+    base: "SauceLabs",
+    browserName: "iphone",
+    platform: "macOS 10.13",
+    version: "latest",
   },
   SL_iOS_1: {
-    base: 'SauceLabs', browserName: 'iphone', platform: 'macOS 10.13', version: 'latest-1',
+    base: "SauceLabs",
+    browserName: "iphone",
+    platform: "macOS 10.13",
+    version: "latest-1",
   },
 };
 
-const reporters = ['dots'];
+const reporters = ["dots"];
 
-if (IS_COVERAGE) reporters.push('coverage');
-if (IS_SAUCE_LABS) reporters.push('saucelabs');
+if (IS_COVERAGE) reporters.push("coverage");
+if (IS_SAUCE_LABS) reporters.push("saucelabs");
 
-module.exports = (config) => {
+module.exports = config => {
   config.set({
-    frameworks: ['jasmine'],
+    frameworks: ["jasmine"],
     reporters,
     browsers: IS_SAUCE_LABS
       ? Object.keys(customLaunchers)
-      : ['ChromeHeadless', 'FirefoxHeadless'],
+      : ["ChromeHeadless", "FirefoxHeadless"],
     files: [
-      'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js',
-      'test/runner.js',
+      "node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js",
+      "test/runner.js",
     ],
     preprocessors: {
-      'test/runner.js': ['webpack', 'sourcemap'],
+      "test/runner.js": ["webpack", "sourcemap"],
     },
     webpack: {
       module: {
         ...webpackConfig.module,
       },
-      devtool: 'inline-source-map',
-      mode: 'development',
+      devtool: "inline-source-map",
+      mode: "development",
       plugins: [
         new webpack.DefinePlugin({
-          'process.env.NODE_ENV': 'env',
+          "process.env.NODE_ENV": "env",
         }),
       ],
     },
     webpackMiddleware: {
       noInfo: true,
-      stats: 'errors-only',
+      stats: "errors-only",
     },
     coverageReporter: {
-      dir: 'coverage/',
+      dir: "coverage/",
       reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
+        { type: "html", subdir: "report-html" },
+        { type: "lcovonly", subdir: ".", file: "lcov.info" },
       ],
     },
     captureTimeout: 120000,
@@ -87,7 +124,7 @@ module.exports = (config) => {
     browserDisconnectTolerance: 2,
     customLaunchers,
     sauceLabs: {
-      testName: 'Hybrids Unit Tests',
+      testName: "Hybrids Unit Tests",
       build: `TRAVIS #${process.env.TRAVIS_BUILD_NUMBER} (${process.env.TRAVIS_BUILD_ID})`,
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
     },
