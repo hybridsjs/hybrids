ea68c77a7c2453d35f1e43d001658b183cda9f15
Benny Powers <bennypowers@users.noreply.github.com>
2020-11-28 18:22:46 +0200

    feat: add types for cache
    fixes #131

--- types/index.d.ts
+++ types/index.d.ts
@@ -151,3 +151,60 @@ declare namespace hybrids {
     function resolve<E extends HTMLElement>(promise: Promise<UpdateFunction<E>>, placeholder?: UpdateFunction<E>, delay?: number) : UpdateFunction<E>;
   }
 }
+
+declare module 'hybrids/esm/cache' {
+  import type { Descriptor } from 'hybrids';
+
+  export interface CacheEntry<T extends HTMLElement = HTMLElement> {
+      target: T,
+      key: keyof T,
+      value: T[keyof T],
+      contexts: unknown,
+      deps: unknown,
+      state: number,
+      checksum: number,
+      observed: boolean
+  }
+
+  export function getEntry<T extends HTMLElement>(
+    target: CacheEntry<T>,
+    key: keyof T
+  ): CacheEntry<T>;
+
+  export function getEntries<T extends HTMLElement>(target: CacheEntry<T>): CacheEntry<T>[];
+
+  export function get<T extends HTMLElement>(
+    target: T,
+    key: keyof T,
+    getter: Descriptor<T>['get'],
+    validate?: <V>(value: V) => boolean
+  ): T[keyof T];
+
+  export function set<T extends HTMLElement>(
+    target: T,
+    key: keyof T,
+    setter: Descriptor<T>['set'],
+    validate?: <V>(value: V) => boolean
+  ): T[keyof T];
+
+  export function invalidate<T extends HTMLElement>(
+    target: T,
+    key: keyof T,
+    clearValue: (...args: any[]) => void,
+    deleteValue: (...args: any[]) => void,
+  ): void
+
+  export function invalidateAll<T extends HTMLElement>(
+    target: T,
+    key: keyof T,
+    clearValue: (...args: any[]) => void,
+    deleteValue: (...args: any[]) => void,
+  ): void
+
+  export function observe<T extends HTMLElement>(
+    target: T,
+    key: keyof T,
+    getter: (key: keyof T) => T[keyof T],
+    fn: (...args: any[]) => void
+  ): () => void
+}
