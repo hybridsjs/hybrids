0ed5156a2990eced8d5016c20c2aabb0aeff797d
Dominik Lubański <dominik.lubanski@gmail.com>
2021-05-20 10:33:39 +0200

    fix(types): render & content type for direct factory usage

--- types/index.d.ts
+++ types/index.d.ts
@@ -37,11 +37,13 @@ declare namespace hybrids {
       keyof Omit<E, keyof HTMLElement>,
       string
     >]: property extends "render" | "content"
-      ? RenderFunction<E> | Property<E, E[property]>
+      ? E[property] extends () => HTMLElement
+        ? RenderFunction<E>
+        : Property<E, E[property]>
       : Property<E, E[property]>;
   } & {
-    render?: RenderFunction<E>;
-    content?: RenderFunction<E>;
+    render?: RenderFunction<E> | Descriptor<E, () => HTMLElement>;
+    content?: RenderFunction<E> | Descriptor<E, () => HTMLElement>;
   };
 
   interface MapOfHybrids {
