049bb018dc67e3a93341205b7391aea25db74672
public202002 <77833368+public202002@users.noreply.github.com>
2023-03-17 08:02:11 +0000

    start

---
+++ script.js
@@ -0,0 +1,15 @@
+import { define, html } from "./src/index.js";
+
+export function increaseCount(host) {
+  host.count += 1;
+}
+
+export default define({
+  tag: "simple-counter",
+  count: 0,
+  render: ({ count }) => html`
+    <button onclick="${increaseCount}">
+      Count: ${count}
+    </button>
+  `
+});
